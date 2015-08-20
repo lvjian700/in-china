dbacb36ad5d3d1eb8504f1fefa1e0bdfb5e6ee27
Jian Lv <jlv@thoughtworks.com>
2015-08-20 15:42:16 +0800

    init project

---
+++ in-china/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  in-china
+//
+//  Created by Jian Lv on 8/20/15.
+//  Copyright (c) 2015 Pricing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ in-china/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  in-china
+//
+//  Created by Jian Lv on 8/20/15.
+//  Copyright (c) 2015 Pricing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
