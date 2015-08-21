1295f19e924c2c4dad831d41e75344be472125a4
Jian Lv <jlv@thoughtworks.com>
2015-08-21 12:30:18 +0800

    [15] refactor speak helper

--- in-china/helpers/SpeakHelper.h
+++ in-china/helpers/SpeakHelper.h
@@ -1,15 +1,7 @@
-//
-//  SpeakHelper.h
-//  in-china
-//
-//  Created by Theesit Konkaew on 8/20/15.
-//  Copyright (c) 2015 Pricing. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface SpeakHelper : NSObject
 
-- (void)speak:(NSString *) textInput;
++ (void)speak:(NSString *) textInput;
 
 @end
--- in-china/helpers/SpeakHelper.m
+++ in-china/helpers/SpeakHelper.m
@@ -1,17 +1,9 @@
-//
-//  SpeakHelper.m
-//  in-china
-//
-//  Created by Theesit Konkaew on 8/20/15.
-//  Copyright (c) 2015 Pricing. All rights reserved.
-//
-
 #import "SpeakHelper.h"
 #import <AVFoundation/AVSpeechSynthesis.h>
 
 @implementation SpeakHelper
 
-- (void)speak:(NSString*) textInput {
++ (void)speak:(NSString*) textInput {
     AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc] initWithString:textInput];
     utterance.voice = [AVSpeechSynthesisVoice voiceWithLanguage:@"zh-CN"];
     
