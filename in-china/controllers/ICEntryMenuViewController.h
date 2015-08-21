fbb7d3c3e1075847da6311ceda17d6d31fcdc323
Jian Lv <jlv@thoughtworks.com>
2015-08-21 13:09:05 +0800

    [13] Add entry menu view

---
+++ in-china/controllers/ICEntryMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  ICEntryMenuViewController.h
+//  in-china
+//
+//  Created by Jian Lv on 8/21/15.
+//  Copyright © 2015 Pricing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICEntryMenuViewController : UIViewController
+
+@end
